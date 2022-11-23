a470609fd06596ac016c3c53163ae90c05bb6a55
khaash15 <101692479+khaash15@users.noreply.github.com>
2022-11-23 14:38:41 +0530

    Update Example.java

--- Exception Handling/Example.java
+++ Exception Handling/Example.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
-public class Test 
-{
+package exceptionHandling; 
+public class ExceptionDivideByZero 
+{ 
 public static void main(String[] args) 
-{
- Scanner sc = new Scanner(System.in);
- System.out.println("Enter two integer numbers");
-
-// Read two integer numbers.
-  int num1 = sc.nextInt();
-  int num2 = sc.nextInt();
-System.out.println(num1 + "/" + num2 + " = " + (num1/num2));
-  }
+{ 
+ System.out.println("One"); 
+ System.out.println("Two"); 
+ 
+ int a = 1/0; // Exceptional case. 
+ 
+ System.out.println("Three"); 
+ System.out.println("Four"); 
+ } 
 }
